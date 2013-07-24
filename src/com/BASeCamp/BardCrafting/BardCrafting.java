a349c11693236a990662f319059d9d92849ff846
Michael Moch <michael.moch@gmail.com>
2013-07-23 21:32:33 -0500

    Recipes cost more, removed nametag recipe.
    I reformatting the code, now it matches my style. And, I renamed the variables so that they follow a more widely accepted naming convention - ClassesGetCapitalized.butVariablesAndMethodNames(alwaysStartWithLowercaseLetters).

--- src/com/BASeCamp/BardCrafting/BardCrafting.java
+++ src/com/BASeCamp/BardCrafting/BardCrafting.java
@@ -8,81 +8,69 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * This is a custom build of BardCrafting. 
+ *
+ * Forked from https://github.com/BCProgramming/BardCrafting. Pray that it
+ * works!
+ *
+ * @author nonzer0value
+ */
 public class BardCrafting extends JavaPlugin {
 
-	ShapedRecipe IronBarding,GoldBarding,DiamondBarding,NameTag;
-	
-	
-	@Override
-	public void onDisable(){
-		this.getLogger().log(Level.ALL, "BardCrafting Disabled.");
-		
-		
-		
-	}
-	@Override
-	public void onEnable(){
-		//shaped recipes for Iron, Gold, and Diamond Armour:
-		this.getLogger().log(Level.ALL, "BardCrafting Enabled.");
-		
-		/*
-		 * IWI
-		 * III
-		 * LLL
-		 * 
-		 * I is material block, W is wool, L is leather.
-		 */
-		
-		
-		
-		IronBarding = new ShapedRecipe(new ItemStack(Material.IRON_BARDING));
-		IronBarding.shape("IWI","III","LGL");
-		IronBarding.setIngredient('I', Material.IRON_BLOCK);
-		IronBarding.setIngredient('W', Material.WOOL);
-		IronBarding.setIngredient('L', Material.LEATHER);
-		IronBarding.setIngredient('G', Material.GOLD_BLOCK);
-		
-		GoldBarding = new ShapedRecipe(new ItemStack(Material.GOLD_BARDING));
-		GoldBarding.shape("GWG","GGG","LDL");
-		GoldBarding.setIngredient('G', Material.GOLD_BLOCK);
-		GoldBarding.setIngredient('W', Material.WOOL);
-		GoldBarding.setIngredient('L', Material.LEATHER);
-		GoldBarding.setIngredient('D', Material.DIAMOND_BLOCK);
-		
-		DiamondBarding = new ShapedRecipe(new ItemStack(Material.DIAMOND_BARDING));
-		DiamondBarding.shape("DWD","DDD","LEL");
-		DiamondBarding.setIngredient('D', Material.DIAMOND_BLOCK);
-		DiamondBarding.setIngredient('W', Material.WOOL);
-		DiamondBarding.setIngredient('L', Material.LEATHER);
-		DiamondBarding.setIngredient('E', Material.EMERALD_BLOCK);
-		
-		//nametag
-		/*
-		 * 
-		 * E
-		 * III
-		 *   L
-		 * 
-		 * E=Emerald, I=Iron Block, L= Leash
-		 */
-		NameTag = new ShapedRecipe(new ItemStack(Material.NAME_TAG));
-		NameTag.shape("  L","III","E  ");
-		NameTag.setIngredient('E', Material.EMERALD);
-		NameTag.setIngredient('I', Material.IRON_BLOCK);
-		NameTag.setIngredient('L', Material.LEASH);
-		
-		
-		
-		
-		//Barding recipes
-		Bukkit.addRecipe(IronBarding);
-		Bukkit.addRecipe(GoldBarding);
-		Bukkit.addRecipe(DiamondBarding);
-		//NameTag
-		Bukkit.addRecipe(NameTag);
-		
-		
-		
-	}
-	
+    ShapedRecipe ironBarding, goldBarding, diamondBarding, nameTag;
+
+    @Override
+    public void onDisable() {
+        this.getLogger().log(Level.ALL, "BardCrafting Disabled.");
+    }
+
+    @Override
+    public void onEnable() {
+        // shaped recipes for Iron, Gold, and Diamond Armour:
+        this.getLogger().log(Level.ALL, "BardCrafting Enabled.");
+
+        // I = Iron Block
+        // W = Wool
+        // E = Emerald Block
+        // G = Gold Block
+        ironBarding = new ShapedRecipe(new ItemStack(Material.IRON_BARDING))
+            .shape(   "IWI"
+                    , "III"
+                    , "GEG")
+            .setIngredient('I', Material.IRON_BLOCK)
+            .setIngredient('W', Material.WOOL)
+            .setIngredient('E', Material.EMERALD_BLOCK)
+            .setIngredient('G', Material.GOLD_BLOCK);
+
+        // G = Gold Block
+        // W = Wool
+        // E = Emerald Block
+        // D = Diamond Block
+        goldBarding = new ShapedRecipe(new ItemStack(Material.GOLD_BARDING))
+            .shape(   "GWG"
+                    , "GGG"
+                    , "DED")
+            .setIngredient('G', Material.GOLD_BLOCK)
+            .setIngredient('W', Material.WOOL)
+            .setIngredient('E', Material.EMERALD_BLOCK)
+            .setIngredient('D', Material.DIAMOND_BLOCK);
+
+        // D = Diamond Block
+        // W = Wool
+        // L = Leather
+        // E = Emerald Block
+        diamondBarding = new ShapedRecipe(new ItemStack(Material.DIAMOND_BARDING))
+            .shape(   "DWD"
+                    , "DDD"
+                    , "DED")
+            .setIngredient('D', Material.DIAMOND_BLOCK)
+            .setIngredient('W', Material.WOOL)
+            .setIngredient('E', Material.EMERALD_BLOCK);
+
+        // Barding recipes
+        Bukkit.addRecipe(IronBarding);
+        Bukkit.addRecipe(GoldBarding);
+        Bukkit.addRecipe(DiamondBarding);
+    }
 }
